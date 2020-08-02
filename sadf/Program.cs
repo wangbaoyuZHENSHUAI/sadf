c0cb9bd1c8af58ad041659c05b38a4272813c1dd
wangbaoyuZHENSHUAI <69064791+wangbaoyuZHENSHUAI@users.noreply.github.com>
2020-08-02 13:43:49 +0800

    添加项目文件。

---
+++ sadf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sadf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
